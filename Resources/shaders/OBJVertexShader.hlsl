62ff14ff468c2b113f6e034eb6d1da436f4ab7e6
MasatuguMatuda <k021g1292@g.neec.ac.jp>
2023-02-03 15:28:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -0,0 +1,25 @@
+#include "OBJShaderHeader.hlsli"
+
+VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
+{
+	//VSOutput output; // ピクセルシェーダーに渡す値
+	//output.svpos = mul(mat, pos);
+	//output.normal = normal;
+	//output.uv = uv;
+	//return output;
+
+	// 右、下、奥の方向を向いたライト
+	float3 lightdir = float3(1, -1, 1);
+	lightdir = normalize(lightdir);
+	// ライトの色（白）
+	float3 lightcolor = float3(1, 1, 1);
+	// ピクセルシェーダーに渡す値
+	VSOutput output;
+	output.svpos = mul(mat, pos);
+	// Lamber反射の計算
+	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.a = m_alpha;
+	output.uv=uv;
+
+	return output;
+}
